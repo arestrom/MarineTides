3e2e23a1cb962714b5e199ab5bc700e40ede201f
arestrom <are.strom@gmail.com>
2024-02-05 12:25:59 -0800

    Trying to quite build checks

--- R/MarineTides-package.R
+++ R/MarineTides-package.R
@@ -9,3 +9,28 @@
 #' Maintainer: Are Strom <are.strom@@gmail.com>
 #' @keywords package
 NULL
+
+## quiets concerns of R CMD check re: the .'s and vars in data.table
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".",
+                                                        "data.table",
+                                                        "timezone",
+                                                        "reference_station_code",
+                                                        "station_datum",
+                                                        "station_meridian",
+                                                        "code",
+                                                        "node_year",
+                                                        "set",
+                                                        "year",
+                                                        "tide_type",
+                                                        "%ilike%",
+                                                        "station_name",
+                                                        "station_code",
+                                                        "tide_type",
+                                                        "tide_time",
+                                                        ":=",
+                                                        "time_offset_high_tide_minutes",
+                                                        "time_offset_low_tide_minutes",
+                                                        "height_offset_factor_low_tide",
+                                                        "height_offset_factor_low_tide",
+                                                        "offset_level",
+                                                        "r_week"))
